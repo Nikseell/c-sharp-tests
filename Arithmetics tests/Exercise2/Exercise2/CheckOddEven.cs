7e05b9acefd8811ef6566333110ba4e31754321c
Nikseell <niksislai@gmail.com>
2021-05-19 13:39:57 +0300

    Exercise2 test completed!

---
+++ Arithmetics tests/Exercise2/Exercise2/CheckOddEven.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise2
+{
+    public class CheckOddEven
+    {
+        public bool EvenOrOdd(int i)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Even Number\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Odd Number\n");
+                return false;
+            }
+        }
+    }
+}
