f5d19e55ed58a68232bcb00c061d3326b38a90f4
Nikseell <niksislai@gmail.com>
2021-05-19 12:28:56 +0300

    Exercise6 test completed!

---
+++ Array tests/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise6
+{
+    public class ReplaceLastArrayElement
+    {
+        static int[] array = new int[10];
+        static Random random = new Random();
+        static int[] array1 = new int[array.Length];
+
+        public int[] MakeArray()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            return array;
+        }
+
+        public int[] ReplaceLastElement()
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = array[i];
+            }
+            array1[array1.Length - 1] = -7;
+
+            return array1;
+        }
+    }
+}
